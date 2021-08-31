daa4a4c9b944782191efb4fc0153bddb37fd9fb0
mtmazzo <74214745+mtmazzo@users.noreply.github.com>
2021-08-30 20:22:53 -0400

    Update balanced-parantheses.js

---
+++ balanced-parantheses.js
@@ -0,0 +1,15 @@
+function areParenthesesBalanced(inputString) {
+    var o = 0;
+    var c = 0;
+    for(var i = 0; i < inputString.length; i++) {
+      if(inputString.charAt(i) == "(") o++;
+      if(inputString.charAt(i) == ")") c++;
+    }
+    return (o == c) ? true : false;
+}
+
+//Returns true
+console.log(areParenthesesBalanced("(()))("));
+
+//Returns false
+console.log(areParenthesesBalanced("(()))()"));
